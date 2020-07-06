a187596fc73bfa5753c0d7bb9afe87349d695d10
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-06 15:09:36 -0700

    08/15 problems

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -12,10 +12,14 @@ x = 5
 y = "7"
 
 # Write a print statement that combines x + y into the integer value 12
-
-# YOUR CODE HERE
+print(x + int(y))
 
 
 # Write a print statement that combines x + y into the string value 57
+# Three ways too print strings
+print(f'{x}{y}')
+print('{}{}'.format( x, y))
+print('%s%s' %(x, y))
 
-# YOUR CODE HERE
+def nums(a, b):
+  print('nums')
