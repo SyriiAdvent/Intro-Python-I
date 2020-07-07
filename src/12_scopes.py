7934aedc2da732691b165ca4d0e10f0f0b209eb8
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-07 14:55:22 -0700

    fix scopes. read the problem too fast!

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -1,3 +1,4 @@
+  
 # Experiment with scopes in Python.
 # Good reading: https://www.programiz.com/python-programming/global-local-nonlocal-variables
 
@@ -5,6 +6,7 @@
 x = 12
 
 def change_x():
+    global x
     x = 99
 
 change_x()
@@ -19,13 +21,15 @@ def outer():
     y = 120
 
     def inner():
+        nonlocal y
         y = 999
-        print(y)
 
     inner()
 
     # This prints 120. What do we have to change in inner() to get it to print
     # 999?
     # Note: Google "python nested function scope".
-    
+    print(y)
+
+
 outer()
